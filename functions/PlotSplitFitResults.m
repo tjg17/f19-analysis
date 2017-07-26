bda08dcf266884dbd75dc8a4fc2ba017c50dcc69
Tyler Glass <tylerglass33@gmail.com>
2017-07-26 15:53:17 -0400

    setup subplot histogram

--- functions/PlotSplitFitResults.m
+++ functions/PlotSplitFitResults.m
@@ -1,18 +1,55 @@
-function [  ] = PlotSplitFitResults( PatientNumber, tau1_normal, tau1_split, tau2_normal, tau2_split, r2_normal, r2_split )
+function [  ] = PlotSplitFitResults( PatientNumber, tau1_normal, tau1_split, tau2_normal, tau2_split, r2_normal, r2_split, mask )
 %Plot results of split fit vs. normal
-figure('Name','Patient 003','NumberTitle','off')
+% mask is needed for r2 map
+
+%Plot results of split fit vs. normal
+figure('Name',strcat('Patient_',num2str(PatientNumber,'%03d')),'NumberTitle','off')
 
 subplot(3,2,1)
+data = tau1_normal;
+histogram_data = data(data>0);
+edges = 0 : 2 : max(data(:));
+histogram(histogram_data,edges)
 title('Tau1 - NORMAL')
 
-C = tau1_map(tau1_map>0);
-edges = [0 : 5 : 100];
-figure
-histogram(C,edges)
-heading = strcat( beginning, ': Wash-in Rate' );
-title(heading)
-xlabel('Tau1 (seconds)')
-ylabel('Count')
+subplot(3,2,2)
+data = tau1_split;
+histogram_data = data(data>0);
+edges = 0 : 2 : max(data(:));
+histogram(histogram_data,edges)
+title('Tau1 - SPLIT')
+
+subplot(3,2,3)
+data = tau2_normal;
+histogram_data = data(data>0);
+edges = 0 : 2 : max(data(:));
+histogram(histogram_data,edges)
+title('Tau2 - NORMAL')
+
+subplot(3,2,4)
+data = tau2_split;
+histogram_data = data(data>0);
+edges = 0 : 2 : max(data(:));
+histogram(histogram_data,edges)
+title('Tau2 - SPLIT')
+
+subplot(3,2,5)
+data = r2_normal;
+data = single(data).*mask/100;
+histogram_data = data(data>0);
+edges = 0.8:0.01:1;
+histogram(histogram_data,edges)
+poorFits = length(find(r2_normal<80));
+title(sprintf('R^2 - NORMAL (%i<0.8)',poorFits))
+
+subplot(3,2,6)
+data = r2_split;
+data = single(data).*mask/100;
+histogram_data = data(data>0);
+edges = 0.8:0.01:1;
+histogram(histogram_data,edges)
+poorFits = length(find(r2_split<80));
+title(sprintf('R^2 - SPLIT (%i<0.8)',poorFits))
 
 end
 
