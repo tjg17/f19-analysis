4ae55083b9c4b4c7a1f1a0e18d4d5d7fc1e51e0a
Tyler Glass <tylerglass33@gmail.com>
2017-07-26 15:54:28 -0400

    loop through split fit patients

--- split_fit.m
+++ split_fit.m
@@ -9,3 +9,25 @@ home = pwd;
 addpath('./functions') % Add path for f19 processing functions
 addpath('G:\2017-Glass\mim\f19_MATLAB_workspaces') % add path for f19 data
 
+%% LDefine Patients and Last PFP Times
+patientNumbers = [ 3; 4; 5; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19; 20];
+last_PFP       = [ 6, 7, 5, 6, 7]; % need to enter all of these
+
+%% Loop Through all F19 Patients
+tic
+for i=1:length(patientNumbers)
+    % Load Patient Data
+    filename = strcat('0509-',num2str(patientNumbers(i),'%03d'),'.mat');
+    load(filename);
+    
+    % Process Normal Fit
+    [tau1_normal , tau2_normal , r2_normal ] = NormalFitProcess( image , roi , scantimes , last_PFP(i) );
+    
+    % Process Split Fit
+    [tau1_split  , tau2_split  , r2_split  ] =  SplitFitProcess( image , roi , scantimes , last_PFP(i) );
+    
+    % Plot Results
+    PlotSplitFitResults( patientNumbers(i), tau1_normal, tau1_split, tau2_normal, tau2_split, r2_normal, r2_split, mask )
+    
+end
+toc
