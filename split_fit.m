d4d7a2aff73d1c26994d402ab53438e7a430429e
Tyler Glass <tylerglass33@gmail.com>
2017-07-27 13:39:34 -0400

    enter first and last PFP points

--- split_fit.m
+++ split_fit.m
@@ -1,7 +1,7 @@
 %% split_fit.m
 % Tyler Glass
 % Code computes time constants and washin/washout dynamics for f19 MRI
-% splits the fit into 2 parts using last_pfp timepoint
+% Splits fitting into separate Washout and Washout Components
 
 %% Initialize Workspace
 clear; clc; close all
@@ -11,8 +11,8 @@ addpath('G:\2017-Glass\mim\f19_MATLAB_workspaces') % add path for f19 data
 
 %% LDefine Patients and Last PFP Times
 patientNumbers = [ 3; 4; 5; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19; 20];
-first_PFP      = [ 1; 2; 2; 2; 2; 2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2];
-last_PFP       = [ 6; 7; 5; 6; 7; 6;  7;  7;  2;  2;  2;  2;  2;  2;  2;  2;  2]; % need to enter all of these
+first_PFP      = [ 1; 2; 2; 1; 2; 2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2];
+last_PFP       = [ 5; 7; 7; 7; 7; 6;  6;  6;  7;  6;  5;  7;  6;  6;  7;  6;  6]; % updated 7/27/17
 
 %% Loop Through all F19 Patients
 tic
@@ -23,13 +23,13 @@ for i=1:length(patientNumbers)
     fprintf('\n\n\nStarting Patient %03d', patientNumbers(i))
     
     % Process Normal Fit
-    [tau1_normal , tau2_normal , r2_normal ] = NormalFitProcess( image , roi , scantimes , last_PFP(i) );
+    [tau1_normal , tau2_normal , r2_normal , mask ] = NormalFitProcess( image , roi , scantimes , last_PFP(i) );
     
     % Process Split Fit
-    [tau1_split  , tau2_split  , r2_split  ] =  SplitFitProcess( image , roi , scantimes , last_PFP(i) );
+    [ df_map , tau1_split , r2_Washin_map , d0_map , tau2_split , r2_Washout_map ] =  SplitFitProcess( image , roi , scantimes , first_PFP(i), last_PFP(i) );
     
     % Plot Results
-    PlotSplitFitResults( patientNumbers(i), tau1_normal, tau1_split, tau2_normal, tau2_split, r2_normal, r2_split, mask )
+    PlotSplitFitResults( patientNumbers(i), tau1_normal, tau1_split, tau2_normal, tau2_split, r2_normal, r2_Washin_map, mask )
     
 end
 toc
