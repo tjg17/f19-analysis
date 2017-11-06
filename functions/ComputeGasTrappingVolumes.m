a48a61a9bf2e232f23aaf2c92b7b21a46a7e5314
Tyler Glass <tylerglass33@gmail.com>
2017-11-06 09:17:46 -0500

    functions for slow filling and gas trapping

---
+++ functions/ComputeGasTrappingVolumes.m
@@ -0,0 +1,72 @@
+function [ GasTrappingVolume , GasTrappingPercentage ] = ComputeGasTrappingVolumes( image , roi , last_pfp , PatientNumber , washoutcycles , ventilated_threshold )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+%% Resample segmentation to size of image volume
+Mask = imresize(roi,[64,64]);
+
+%% Choose Image Timepoint to Compute Gas Trapping
+GasTrapImage = image(:,:,:,last_pfp+washoutcycles).*Mask; % after 3 washout cycles
+
+%% Create Binary Image using Threshold
+GasTrapBinary = zeros(size(GasTrapImage));
+for slice = 1:size(GasTrapImage,3)
+    GasTrapBinary(:,:,slice) = imbinarize(GasTrapImage(:,:,slice), ventilated_threshold); % make binary image base on ventilation threshold
+end
+
+%% Clean Up Binary Image to Remove Small Volumes
+GasTrapBinary = bwareaopen(GasTrapBinary,3);
+
+%% Compute Gas Trapping and Ventilation Volumes
+GasTrappingVolume = sum(GasTrapBinary(:))*6.25*6.25*15/1e6; % in liters
+VentilationVolume = sum(Mask(:))*6.25*6.25*15/1e6; % in liters
+GasTrappingPercentage = GasTrappingVolume/VentilationVolume; % percent
+
+% %% Optional Plotting
+% figure(PatientNumber);clf
+% plot_title = sprintf('Gas Trapping for Patient %i',PatientNumber);
+% 
+% subplot(4,4,1)
+% imshow(GasTrapBinary(:,:,2),[])
+% title(plot_title)
+% subplot(4,4,2)
+% imshow(GasTrapBinary(:,:,3),[])
+% subplot(4,4,3)
+% imshow(GasTrapBinary(:,:,4),[])
+% subplot(4,4,4)
+% imshow(GasTrapBinary(:,:,5),[])
+% subplot(4,4,5)
+% imshow(GasTrapBinary(:,:,6),[])
+% subplot(4,4,6)
+% imshow(GasTrapBinary(:,:,7),[])
+% subplot(4,4,7)
+% imshow(GasTrapBinary(:,:,8),[])
+% subplot(4,4,8)
+% imshow(GasTrapBinary(:,:,9),[])
+% subplot(4,4,9)
+% imshow(GasTrapBinary(:,:,10),[])
+% subplot(4,4,10)
+% imshow(GasTrapBinary(:,:,11),[])
+% subplot(4,4,11)
+% imshow(GasTrapBinary(:,:,12),[])
+% subplot(4,4,12)
+% imshow(GasTrapBinary(:,:,13),[])
+% subplot(4,4,13)
+% imshow(GasTrapBinary(:,:,14),[])
+% subplot(4,4,14)
+% imshow(GasTrapBinary(:,:,15),[])
+% subplot(4,4,15)
+% imshow(GasTrapBinary(:,:,16),[])
+% subplot(4,4,16)
+% imshow(GasTrapBinary(:,:,17),[])
+% 
+% pause(2)
+% 
+% % Save figure
+% FigureDirectory    = strcat('G:/2017-Glass/f19_fit_results/GasTrapping/');    mkdir(FigureDirectory);
+% FigureName = strcat('GasTrapping_Patient',string(PatientNumber));
+% FileName = char(strcat(FigureDirectory,FigureName,'.png'));
+% saveas(gcf,FileName)
+
+end
+
---
+++ functions/ComputeSlowFillingVolumes.m
@@ -0,0 +1,80 @@
+function [ SlowFillingVolume , SlowFillingPercentage ] = ComputeSlowFillingVolumes( image , roi , first_PFP , PatientNumber, washincycles , unventilated_threshold )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+%% Resample segmentation to size of image volume
+Mask = imresize(roi,[64,64]);
+
+%% Choose Image Timepoint to Compute Gas Trapping
+SlowFillingImage = image(:,:,:,first_PFP+washincycles-1).*Mask; % firstPFP includes 1 washin cycle
+
+%% Create Binary Image using Threshold
+% this is fast fill as currently constructed...
+SlowFillingImage(find(SlowFillingImage<0.5)) = 100; % set outside lung to some high value
+SlowFillingImage(find(SlowFillingImage>unventilated_threshold)) = 0; % set all values greater than threshold to 0
+SlowFillBinary = SlowFillingImage; SlowFillBinary(find(SlowFillBinary>0.5)) = 1;
+
+% SlowFillBinary = zeros(size(SlowFillingImage));
+% for slice = 1:size(SlowFillingImage,3)
+%     SlowFillBinary(:,:,slice) = imbinarize(SlowFillingImage(:,:,slice), unventilated_threshold); % make binary image base on ventilation threshold
+% end
+
+
+%% Clean Up Binary Image to Remove Small Volumes
+%SlowFillBinary = bwareaopen(SlowFillBinary,3); % num pixels to keep image
+
+%% Compute Gas Trapping and Ventilation Volumes
+VentilationVolume = sum(Mask(:))*6.25*6.25*15/1e6; % in liters
+
+SlowFillingVolume = sum(SlowFillBinary(:))*6.25*6.25*15/1e6; % in liters
+
+SlowFillingPercentage = SlowFillingVolume/VentilationVolume; % percent
+
+%% Optional Plotting
+figure(PatientNumber);clf
+plot_title = sprintf('Slow Filling for Patient %i',PatientNumber);
+
+subplot(4,4,1)
+imshow(SlowFillBinary(:,:,2),[])
+title(plot_title)
+subplot(4,4,2)
+imshow(SlowFillBinary(:,:,3),[])
+subplot(4,4,3)
+imshow(SlowFillBinary(:,:,4),[])
+subplot(4,4,4)
+imshow(SlowFillBinary(:,:,5),[])
+subplot(4,4,5)
+imshow(SlowFillBinary(:,:,6),[])
+subplot(4,4,6)
+imshow(SlowFillBinary(:,:,7),[])
+subplot(4,4,7)
+imshow(SlowFillBinary(:,:,8),[])
+subplot(4,4,8)
+imshow(SlowFillBinary(:,:,9),[])
+subplot(4,4,9)
+imshow(SlowFillBinary(:,:,10),[])
+subplot(4,4,10)
+imshow(SlowFillBinary(:,:,11),[])
+subplot(4,4,11)
+imshow(SlowFillBinary(:,:,12),[])
+subplot(4,4,12)
+imshow(SlowFillBinary(:,:,13),[])
+subplot(4,4,13)
+imshow(SlowFillBinary(:,:,14),[])
+subplot(4,4,14)
+imshow(SlowFillBinary(:,:,15),[])
+subplot(4,4,15)
+imshow(SlowFillBinary(:,:,16),[])
+subplot(4,4,16)
+imshow(SlowFillBinary(:,:,17),[])
+
+
+% % Save figure
+FigureDirectory    = strcat('G:/2017-Glass/f19_fit_results/SlowFilling2cycles/');    mkdir(FigureDirectory);
+FigureName = strcat('SlowFilling2_Patient',string(PatientNumber));
+FileName = char(strcat(FigureDirectory,FigureName,'.png'));
+saveas(gcf,FileName)
+
+
+end
+
