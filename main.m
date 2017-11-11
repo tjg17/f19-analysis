753fea573780cf3c8fa9f725eebf8acd79f86bf7
Tyler Glass <tylerglass33@gmail.com>
2017-11-11 14:01:52 -0500

    update first and last PFP frames

--- main.m
+++ main.m
@@ -6,24 +6,27 @@
 clear; clc; close all
 home = pwd;
 addpath('./functions') % Add path for f19 processing functions
-addpath('G:\2017-Glass\mim\f19_MATLAB_workspaces') % add path for f19 data
+
+%% Choose Data Directory
+%addpath('G:\2017-Glass\mim\f19_MATLAB_workspaces') % add path for f19 data
+addpath('G:\2017-Glass\mim\f19_ventilation_segmentations')
 
 %% Data for First and Last PFP Times
-patientNumbers = [ 3; 4; 5; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19; 20];
-first_PFP      = [ 1; 2; 2; 1; 2; 2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2];
-last_PFP       = [ 5; 7; 7; 7; 7; 6;  6;  6;  7;  6;  5;  7;  6;  6;  7;  6;  6]; % updated 7/27/17
+patientNumbers = [2; 3; 4; 5; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19; 20; 21; 22; 24; 25];
+first_PFP      = [2; 1; 2; 2; 1; 2; 2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2];
+last_PFP       = [7; 5; 7; 7; 7; 7; 6;  6;  6;  7;  6;  5;  7;  6;  6;  7;  6;  6;  6;  6;  4;  6]; % updated 11/11/2017
 
 %% Select Patients for Experiment to Run
-ExperimentName  = 'd0_constant';
-FigureDirectory    = strcat('../../f19_fit_results/',ExperimentName,'/figures/');    mkdir(FigureDirectory);
-ParameterDirectory = strcat('../../f19_fit_results/',ExperimentName,'/parameters/'); mkdir(ParameterDirectory);
+%ExperimentName  = 'd0_constant';
+%FigureDirectory    = strcat('../../f19_fit_results/',ExperimentName,'/figures/');    mkdir(FigureDirectory);
+%ParameterDirectory = strcat('../../f19_fit_results/',ExperimentName,'/parameters/'); mkdir(ParameterDirectory);
 
-% patientNumbers  = [ 3; 4; 5; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19; 20];
-% first_PFP       = [ 1; 2; 2; 1; 2; 2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2;  2];
-% last_PFP        = [ 5; 7; 7; 7; 7; 6;  6;  6;  7;  6;  5;  7;  6;  6;  7;  6;  6]; 
+Control = [ 2; 3; 4;  5; 11; 15; 16; 17]; % check on 19,20
+Exper   = [ 7; 8; 9; 10; 12; 13; 14; 18; 19; 20]; % not sure on 19,20 but both look normal
 
-Control = [ 3; 4; 5; 11; 15; 16; 17];
-Exper   = [ 7; 8; 9; 10; 12; 13; 14; 18]; % not sure on 19,20
+% Set up counters for control and exper
+ControlCount = 1;
+ExperCount = 1;
 
 %% Loop Through all F19 Patients
 tic
@@ -31,12 +34,12 @@ for i=1:length(patientNumbers)
     % Load Patient Data
     filename = strcat('0509-',num2str(patientNumbers(i),'%03d'),'.mat');
     load(filename);
-    fprintf('\n\n\nStarting Patient %03d', patientNumbers(i))
+    %fprintf('\n\n\nStarting Patient %03d', patientNumbers(i))
     
     % Process Normal Fit
     %[tau1_normal , tau2_normal , r2_normal , mask ] = NormalFitProcess( image , roi , scantimes , last_PFP(i) );
     
-    % Process Split Fit
+    %Process Split Fit
     %[ dF , tau1 , r2_Washin , d0 , tau2 , r2_Washout ] =  SplitFitProcess( image , roi , scantimes , first_PFP(i), last_PFP(i) );
     
     % Plot Results
@@ -45,16 +48,43 @@ for i=1:length(patientNumbers)
     % Save Parameters
     %SaveFitParameterData(patientNumbers(i), ParameterDirectory, tau1, tau2, dF, d0)
     %VentilationVolumeLiters(i,1) = patientNumbers(i);
-    %Mask = imresize(roi,[64,64]);
-    %VentilationVolumeLiters(i,2) = sum(Mask(:))*6.25*6.25*15/1e6;
-    backgroundslice1 = image(:,:,:,1); backgroundslice1 = backgroundslice1(:);
-    backgroundslice2 = image(:,:,:,end); backgroundslice2 = backgroundslice2(:);
-    means1(i) = mean(backgroundslice1);
-    means2(i) = mean(backgroundslice2);
-    noise1(i) = std(backgroundslice1);
-    noise2(i) = std(backgroundslice2);
+    Mask = imresize(roi,[64,64]);
+    %VentilationVolumeLiters(i,1) = patientNumbers(i);
+    VentilationVolumeLiters(i) = sum(Mask(:))*6.25*6.25*15/1e6;
+    
+    % Compute Slow Washins
+    [SlowFillingVolumes(i) , SlowFillingPercentages(i) ] = ComputeSlowFillingVolumes(image, roi, first_PFP(i) , patientNumbers(i) , 2 , 11); % 2 washin cycles, threshold 11
+    
+    % Compute Gas Trapping
+    [GasTrappingVolumes(i) , GasTrappingPercentages(i) ] = ComputeGasTrappingVolumes(image, roi, last_PFP(i)  , patientNumbers(i) , 3 , 11); % 3 washout cycles, threshold 11
+    
+    % Compute Total Ventilation Number
+    MaximumValsImage = max(image,[],4);
+    TotalVentilationVolume = Mask.*MaximumValsImage;
+    TotalVentilation(i) = sum(TotalVentilationVolume(:))*6.25*6.25*15/1e6;
+    
+    % Compute Stats
+    if ismember(patientNumbers(i),Control)
+        Control_VentilationVolume(ControlCount) = VentilationVolumeLiters(i);
+        Control_SlowFillPercent(ControlCount) = SlowFillingPercentages(i);
+        Control_TrapPercent(ControlCount) = GasTrappingPercentages(i);
+        ControlCount = ControlCount + 1;
+    elseif ismember(patientNumbers(i),Exper)
+        Exper_VentilationVolume(ExperCount) = VentilationVolumeLiters(i);
+        Exper_SlowFillPercent(ExperCount) = SlowFillingPercentages(i);
+        Exper_TrapPercent(ExperCount) = GasTrappingPercentages(i);
+        ExperCount = ExperCount + 1;
+    end
+       
     
 end
+
+% loop for stats
+[h_ventVolu , p_ventVolu] = ttest2(Control_VentilationVolume , Exper_VentilationVolume , 'Vartype','unequal')
+[h_trapPerc , p_TrapPerc] = ttest2(Control_TrapPercent , Exper_TrapPercent , 'Vartype','unequal' )
+[h_slowFPer , p_slowFPer] = ttest2(Control_TrapPercent , Exper_TrapPercent , 'Vartype','unequal' )
+%[h_trapNorm , p_TrapNorm] = ttest2(Control_TrapNormalized , Exper_TrapNormalized , 'Vartype','unequal' )
+
 toc
-%VentilationVolumeLiters
-%mean(VentilationVolumeLiters(:,2))
+
+%SlowFillingPercentages'
