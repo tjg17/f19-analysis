234a08b710996b3c4a3c2e47b0ef6cfc9f2405e7
Tyler Glass <tylerglass33@gmail.com>
2017-12-08 15:04:58 -0500

    added pixel size and slice thickness to overlap combined volume module

--- functions/ComputeCombinedOverlapVolumes.m
+++ functions/ComputeCombinedOverlapVolumes.m
@@ -1,13 +1,13 @@
-function [ OverlapVolume , CombinedVolume ] = ComputeCombinedOverlapVolumes( volume1 , volume2 )
+function [ OverlapVolume , CombinedVolume ] = ComputeCombinedOverlapVolumes( volume1 , volume2 , pixel_size , slice_thickness )
 %Computes overlapping and combined volumes for input 3d binary masks
 % inputs must be same size matrices
 
 %% Find Overlapping Volumes
 OverlapMask = and(volume1,volume2);
-OverlapVolume = sum(OverlapMask(:))*6.25*6.25*15/1e6; % in liters
+OverlapVolume = sum(OverlapMask(:))*pixel_size*pixel_size*slice_thickness;
 
 %% Find Combined Volumes
 CombinedMask = or(volume1,volume2);
-CombinedVolume = sum(CombinedMask(:))*6.25*6.25*15/1e6; % in liters
+CombinedVolume = sum(CombinedMask(:))*pixel_size*pixel_size*slice_thickness;
 
 end
